cb8035947247e2f7b086e10abfd5bde517ff9c31
DUNYU LIU <43709243+dunyuliu@users.noreply.github.com>
2024-09-14 10:37:03 -0500

    Update install.env.sh

--- install.env.sh
+++ install.env.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-apt-get install git vim python3 python3-pip python3-matplotlib gmt
+apt-get install git vim python3 python3-pip python3-matplotlib gmt python3-numpy
 apt-get install python3-gi python3-gi-cairo gir1.2-gtk-4.0 libgtk-4-dev
 apt-get install build-essential
 apt-get install gfortran
