8ac2e7020d4e49d2bc56eb1c20be542a1ecc1cd1
DUNYU LIU <43709243+dunyuliu@users.noreply.github.com>
2024-09-20 16:41:45 -0500

    Update and rename install.env.sh to install.ubuntu22.env.sh

--- install.ubuntu22.env.sh
+++ install.ubuntu22.env.sh
@@ -3,4 +3,5 @@ apt-get install git vim python3 python3-pip python3-matplotlib gmt python3-numpy
 apt-get install python3-gi python3-gi-cairo gir1.2-gtk-4.0 libgtk-4-dev
 apt-get install build-essential
 apt-get install gfortran
+pip install --user matplotlib==3.9.2 # needs newer version of matplotlib to work.
 
