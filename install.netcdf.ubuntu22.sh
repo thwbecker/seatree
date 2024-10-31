19dbcb60efa30630e3d18229993bfad7b93499a5
DUNYU LIU <43709243+dunyuliu@users.noreply.github.com>
2024-10-31 05:33:12 -0500

    Update install.netcdf.ubuntu22.sh

--- install.netcdf.ubuntu22.sh
+++ install.netcdf.ubuntu22.sh
@@ -1,7 +1,7 @@
 VERSION="4.9.3-rc1"
 SEATREEROOT=$(pwd)
 echo "SEATREE ROOT path is" $SEATREEROOT
-#wget https://github.com/Unidata/netcdf-c/archive/refs/tags/v$VERSION.tar.gz
+wget https://github.com/Unidata/netcdf-c/archive/refs/tags/v$VERSION.tar.gz
 tar -zxvf v$VERSION.tar.gz
 NETCDF_DIR_NAME=netcdf-c-$VERSION
 cd $NETCDF_DIR_NAME
