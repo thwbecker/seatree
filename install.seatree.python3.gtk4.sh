13d6be6f80ba0fab9ca7ba89cfeddb1fb2eabf35
DUNYU LIU <43709243+dunyuliu@users.noreply.github.com>
2024-11-04 10:33:16 -0600

    finalizing

--- install.seatree.python3.gtk4.sh
+++ install.seatree.python3.gtk4.sh
@@ -3,7 +3,7 @@
 # The shell script is to set up environment for SEATREE.
 # Currently supported systems include:
 # 1. ubuntu: Ubuntu 22.04
-# 2. docker: ubuntu 22.04 with docker.
+# 2. docker: please see Dockerfile.
 # 3. macos: MacOS 14
 
 while getopts "m:e:c:h" OPTION; do
@@ -15,30 +15,26 @@ while getopts "m:e:c:h" OPTION; do
             MACH=$OPTARG
             ENV="True"
             ;;
-        c)
-            MACH=$OPTARG
-            CONFIG="True"
-            ;;
         h)
-            echo "Usage: ./install.seatree.sh [-h] [-m Machine_name] [-c Machine_name] "
+            echo "Usage: ./install.seatree.python3.gtk4.sh [-h] [-m Machine_name] [-e Machine_name] "
             echo "                                                                     "
             echo "Examples:                                                            "
             echo "                                                                     "
-            echo "./install.seatree.sh -h                                               "
+            echo "./install.seatree.python3.gtk4.sh -h                                 "
             echo " -----Display this help message                                      "
             echo "                                                                     "
-            echo "./install.seatree.sh -m ubuntu                                           "
-            echo " -----Install SEATREE on ubuntu 22.04                            "
+            echo "./install.seatree.python3.gtk4.sh -m ubuntu                          "
+            echo " -----Install SEATREE on ubuntu 22.04                                "
             echo "                                                                     "
-            echo "./install.seatree.sh -c macos                                        "
-            echo " -----Simply set up envs for SEATREE without installation             "
-            echo " -----on macos                                                      "
+            echo "./install.seatree.python3.gtk4.sh -e macos                           "
+            echo " -----Simply set up envs for SEATREE without installation            "
+            echo " -----on macos                                                       "
             echo "                                                                     "
-            echo "source install.seatree.sh                                             "
-            echo " -----Activate ENV VAR EQQUASIROOT and add exes to PATH              "
+            echo "source ./install.seatree.python3.gtk4.sh                             "
+            echo " -----Activate ENV VAR and add exes to PATH                          "
             echo "                                                                     "
             echo "Currently supported machines include:                                "
-            echo " ubuntu/macos/docker                                                    "
+            echo " ubuntu/macos/docker                                                 "
             ;;
     esac
 done 
@@ -47,19 +43,10 @@ if [ -n "$MACH" ]; then
     export MACHINE=$MACH
     if [ $MACHINE == "ubuntu" ]; then 
         if [ -n "$ENV" ]; then
-          sudo apt-get install git vim python3 python3-pip python3-matplotlib gmt python3-numpy
+          sudo apt-get install git wget cmake python3 python3-pip python3-numpy
           sudo apt-get install python3-gi python3-gi-cairo gir1.2-gtk-4.0 libgtk-4-dev
-          sudo apt-get install build-essential
-          sudo apt-get install gfortran
-          pip3 install --user matplotlib==3.9.2 --break-system-packages # needs newer version of matplotlib to work.
-        fi 
-    elif [ $MACHINE == "docker" ]; then 
-        echo "Installing SEATREE on Docker Ubuntu 22.04 ... ..."
-        if [ -n "$ENV" ]; then
-          apt-get install git vim python3 python3-pip python3-matplotlib gmt python3-numpy
-          apt-get install python3-gi python3-gi-cairo gir1.2-gtk-4.0 libgtk-4-dev
-          apt-get install build-essential
-          apt-get install gfortran
+          sudo apt-get install build-essential gfortran
+          sudo apt-get install x11-apps libx11-dev libxt-dev libxaw7-dev ghostscript libhdf5-dev gawk
           pip3 install --user matplotlib==3.9.2 --break-system-packages # needs newer version of matplotlib to work.
         fi 
     elif [ $MACHINE == "macos" ]; then 
@@ -74,7 +61,22 @@ if [ -n "$MACH" ]; then
     export GMT4HOME=$(pwd)/gmt-4.5.18
     export GMTHOME=$GMT4HOME
     export NETCDFHOME=$(pwd)/netcdf-c-4.9.3-rc1
+
+    if [ -e "netcdf-c-4.9.3-rc1" ]; then 
+        echo "Seems netcdf-c-4.9.3-rc1 has been installed."
+    else
+        echo "Local netcdf is not available, installing netcdf-c-4.9.3-rc1 ..."
+        bash install.netcdf.ubuntu22.sh
+    fi 
+    if [ -e "gmt-4.5.18" ]; then  
+        echo "Seems gmt-4.5.18 has been installed."
+    else 
+        echo "Local gmt4 is not available, installing gmt4-5.18 ..."
+        bash install.gmt4.ubuntu22.sh
+    fi 
     yes '' | ./configure.python3.gtk4 
+
+    echo "Installing new ConMan v3.0.0 from CIG ConMan GitHub ..."
     bash install.conman.sh
 fi
 
