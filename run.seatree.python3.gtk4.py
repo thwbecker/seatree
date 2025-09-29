00cb44c4a975364f4d6fbdc513b026709b08c62b
DUNYU LIU <43709243+dunyuliu@users.noreply.github.com>
2025-09-29 00:29:09 -0500

    add ENV support to GMT and NETCDF in run script; add build.sh for distribution

--- run.seatree.python3.gtk4.py
+++ run.seatree.python3.gtk4.py
@@ -10,6 +10,32 @@ def add_subdirectories_to_path(root_dir):
 root_directory = os.getcwd()
 add_subdirectories_to_path(root_directory)
 
+# Set up GMT environment variables for direct execution
+gmt_home = os.path.join(root_directory, "gmt-4.5.18")
+if os.path.exists(gmt_home):
+    os.environ["GMT4HOME"] = gmt_home
+    os.environ["GMTHOME"] = gmt_home
+    os.environ["GMT_SHAREDIR"] = os.path.join(gmt_home, "share")
+    os.environ["GMT_DATADIR"] = os.path.join(gmt_home, "share")
+    os.environ["GMT_FONTPATH"] = os.path.join(gmt_home, "share", "pslib")
+
+    # Add GMT to PATH
+    gmt_bin = os.path.join(gmt_home, "bin")
+    if gmt_bin not in os.environ.get("PATH", ""):
+        os.environ["PATH"] = gmt_bin + os.pathsep + os.environ.get("PATH", "")
+
+# Set up NetCDF environment variables if needed
+netcdf_home = os.path.join(root_directory, "netcdf-c-4.9.3-rc1")
+if os.path.exists(netcdf_home):
+    os.environ["NETCDFHOME"] = netcdf_home
+
+    # Add NetCDF lib to LD_LIBRARY_PATH
+    netcdf_lib = os.path.join(netcdf_home, "lib")
+    if "LD_LIBRARY_PATH" in os.environ:
+        os.environ["LD_LIBRARY_PATH"] = netcdf_lib + os.pathsep + os.environ["LD_LIBRARY_PATH"]
+    else:
+        os.environ["LD_LIBRARY_PATH"] = netcdf_lib
+
 #for path in sys.path:
 #    print(path)
 
